397e7b1721dbad6d4312918d7c9db74dd0e941d4
arnold <67833474+luizarnoldch@users.noreply.github.com>
2022-11-11 20:43:10 -0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home(params) {
+  return (
+    <div className="text-white">
+      Index
+    </div>
+  )
+};
---
+++ app/prueba/page.jsx
@@ -0,0 +1,7 @@
+export default function Prueba(params) {
+  return (
+    <div className="text-white">
+      Prueba
+    </div>
+  )
+}
